4c9db008332c79b1a10ce1b0d310239cc67e4477
Luigi Montanez <luigi.montanez@gmail.com>
2009-08-19 09:54:16 -0400

    Raise exceptions on HTTP error status codes.

--- lib/datacatalog.rb
+++ lib/datacatalog.rb
@@ -4,6 +4,11 @@ require 'activesupport'
 
 module DataCatalog
   mattr_accessor :api_key, :base_uri
+  class ApiKeyUndefined < RuntimeError; end
+  class BadRequest < RuntimeError; end            # 400
+  class RequestUnauthorized < RuntimeError; end   # 401
+  class ResourceNotFound < RuntimeError; end      # 404
+  class InternalServerError < RuntimeError; end   # 500
 end
 
 Dir["#{File.dirname(__FILE__)}/datacatalog/*.rb"].each { |source_file| require source_file }
