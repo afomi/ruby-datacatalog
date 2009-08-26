6b53786c81a1eb162ff9ff151691cb898dc55377
Luigi Montanez <luigi.montanez@gmail.com>
2009-08-26 17:12:24 -0400

    Remove monkeypatch of OpenStruct.

--- lib/datacatalog/base.rb
+++ lib/datacatalog/base.rb
@@ -51,29 +51,6 @@ module DataCatalog
       instance_values["table"][:id]
     end
     
-    # Tweak behavior of OpenStruct
-    # so that method calls that don't exist explode
-    # rather than returning nil
-    def method_missing(mid, *args) # :nodoc:
-      mname = mid.id2name
-      len = args.length
-      if mname =~ /=$/
-        if len != 1
-          raise ArgumentError, "wrong number of arguments (#{len} for 1)", caller(1)
-        end
-        if self.frozen?
-          raise TypeError, "can't modify frozen #{self.class}", caller(1)
-        end
-        mname.chop!
-        self.new_ostruct_member(mname)
-        @table[mname.intern] = args[0]
-      elsif len == 0 && @table[mid]
-        @table[mid]
-      else
-        raise NoMethodError, "undefined method `#{mname}' for #{self}", caller(1)
-      end
-    end
-
   end # class Base
 
 end # module DataCatalog
