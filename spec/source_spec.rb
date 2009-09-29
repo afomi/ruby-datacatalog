2468a4d2b46c35f968be6817d1e15be083d226cf
David James <djames@sunlightfoundation.com>
2009-09-29 15:46:31 -0400

    Rearranging source_spec

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -1,6 +1,13 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe DataCatalog::Source do
+
+    def create_source
+    DataCatalog::Source.create({
+      :title => "Some FCC Data",
+      :url   => "http://fcc.gov/somedata.csv"
+    })
+  end
   
   before(:each) do
     setup_api
@@ -28,11 +35,9 @@ describe DataCatalog::Source do
       expected = ["FCC Data", "NASA Data", "DOE Data"]
       @sources.map(&:title).sort.should == expected.sort
     end
-    
   end # describe ".all"
   
   describe ".create" do
-    
     it "should create a new source when valid params are passed in" do
       source = DataCatalog::Source.create({
         :title => "Some FCC Data",
@@ -41,43 +46,37 @@ describe DataCatalog::Source do
       source.should be_an_instance_of(DataCatalog::Source)
       source.url.should == "http://fcc.gov/somedata.csv"
     end
-    
   end # describe ".all"
-  
+
   describe ".update" do
-    
+    before do
+      @source = create_source
+    end
+
     it "should update an existing source when valid params are passed in" do
-      new_source = DataCatalog::Source.create({
-        :title => "Some FCC Data",
-        :url   => "http://fcc.gov/somedata.csv"
-      })
-      source = DataCatalog::Source.update(new_source.id, {
+      source = DataCatalog::Source.update(@source.id, {
         :url => "http://fec.gov/newdata.csv"
       })
       source.should be_an_instance_of(DataCatalog::Source)
       source.url.should == "http://fec.gov/newdata.csv"
     end
-    
   end # describe ".all"  
   
   describe ".destroy" do
-    
+    before do
+      @source = create_source
+    end
+
     it "should destroy an existing source" do
-      source = DataCatalog::Source.create({
-        :title => "Some FCC Data",
-        :url   => "http://fcc.gov/somedata.csv"
-      })
-      result = DataCatalog::Source.destroy(source.id)
+      result = DataCatalog::Source.destroy(@source.id)
       result.should be_true
     end
     
     it "should raise NotFound when attempting to destroy non-existing source" do
-      source_id = "000000000000000000000000"
       executing do
-        DataCatalog::Source.destroy(source_id)
+        DataCatalog::Source.destroy(mangle(@source.id))
       end.should raise_error(DataCatalog::NotFound)
     end
-    
   end # describe ".destroy"
   
 end
