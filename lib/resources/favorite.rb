4aa60dced20f7baa4311ed23ea1ddbb556ab2a08
Luigi Montanez <luigi.montanez@gmail.com>
2009-11-16 18:12:37 -0500

    Support for favorites [Story1735973]

---
+++ lib/resources/favorite.rb
@@ -0,0 +1,29 @@
+module DataCatalog
+
+  class Favorite < Base
+    
+    def self.all(conditions={})      
+      many(http_get(uri, :query => query_hash(conditions)))
+    end
+
+    def self.create(params={})
+      one(http_post(uri, :body => params))
+    end
+
+    def self.destroy(id)
+      one(http_delete(uri(id)))
+    end
+    
+    def self.get(id)
+      one(http_get(uri(id)))
+    end
+
+    # == Helpers
+    
+    def self.uri(id=nil)
+      "/favorites/#{id}"
+    end
+
+  end
+
+end
