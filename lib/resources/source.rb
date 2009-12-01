e6938bc20dd0595a57f59393c260213c05fefe7e
David James <djames@sunlightfoundation.com>
2009-12-01 10:11:06 -0500

    minor: whitespace

--- lib/resources/source.rb
+++ lib/resources/source.rb
@@ -5,11 +5,11 @@ module DataCatalog
     def self.all(conditions={})      
       many(http_get(uri, :query => query_hash(conditions)))
     end
-    
+
     def self.search(term)
-      many(http_get(uri, :query => {:search => term.downcase}))
+      many(http_get(uri, :query => { :search => term.downcase }))
     end
-    
+
     def self.create(params={})
       one(http_post(uri, :body => params))
     end
@@ -17,21 +17,21 @@ module DataCatalog
     def self.destroy(id)
       one(http_delete(uri(id)))
     end
-    
-    def self.first(conditions= {})
+
+    def self.first(conditions={})
       one(http_get(uri, :query => query_hash(conditions)).first)
     end
 
     def self.get(id)
       one(http_get(uri(id)))
     end
-    
+
     def self.update(id, params={})
       one(http_put(uri(id), :body => params))
     end
 
     # == Helpers
-    
+
     def self.uri(id=nil)
       "/sources/#{id}"
     end
