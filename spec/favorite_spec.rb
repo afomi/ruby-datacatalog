4a77d177b136b867b1e4fe926de962dbbe72f0af
David James <djames@sunlightfoundation.com>
2010-08-10 16:10:22 -0400

    minor: alphabetize

--- spec/favorite_spec.rb
+++ spec/favorite_spec.rb
@@ -20,10 +20,37 @@ describe Favorite do
     end
   end
 
+  describe ".all" do
+    it "should return an enumeration of favorites" do
+      favorites = Favorite.all(:user_id => @user.id)
+      favorites.each do |o|
+        o.should be_an_instance_of(Favorite)
+      end
+    end
+  end
+
   describe ".create" do
     it "should create a favorite when valid params are passed in" do
       refreshed_user = User.get(@user.id)
       refreshed_user.favorites.first.title.should == "Some FCC Data"
+    end
+  end
+
+  describe ".destroy" do
+    it "should destroy an existing favorite as an admin" do
+      Favorite.destroy(@favorite.id).should be_true
+    end
+
+    it "should destroy an existing favorite as the user" do
+      DataCatalog.with_key(@user.primary_api_key) do
+        Favorite.destroy(@favorite.id).should be_true
+      end
+    end
+
+    it "should raise NotFound when attempting to destroy non-existing favorite" do
+      executing do
+        Favorite.destroy(mangle(@favorite.id))
+      end.should raise_error(NotFound)
     end
   end
 
@@ -43,33 +70,6 @@ describe Favorite do
     it "should raise NotFound if no favorite exists" do
       executing do
         Favorite.get(mangle(@favorite.id))
-      end.should raise_error(NotFound)
-    end
-  end
-
-  describe ".all" do
-    it "should return an enumeration of favorites" do
-      favorites = Favorite.all(:user_id => @user.id)
-      favorites.each do |o|
-        o.should be_an_instance_of(Favorite)
-      end
-    end
-  end
-
-  describe ".destroy" do
-    it "should destroy an existing favorite as an admin" do
-      Favorite.destroy(@favorite.id).should be_true
-    end
-
-    it "should destroy an existing favorite as the user" do
-      DataCatalog.with_key(@user.primary_api_key) do
-        Favorite.destroy(@favorite.id).should be_true
-      end
-    end
-
-    it "should raise NotFound when attempting to destroy non-existing favorite" do
-      executing do
-        Favorite.destroy(mangle(@favorite.id))
       end.should raise_error(NotFound)
     end
   end
