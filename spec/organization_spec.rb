019330c44ab3110de0e9ee33d72b68ff67102842
David James <djames@sunlightfoundation.com>
2010-08-10 16:51:15 -0400

    minor: alphabetize

--- spec/organization_spec.rb
+++ spec/organization_spec.rb
@@ -40,15 +40,33 @@ describe Organization do
     end
   end
 
-  describe ".search" do
-    before do
-      create_3_organizations
+  describe ".create" do
+    it "should create a new organization when valid params are passed in" do
+      org = Organization.create(:name => "Federal Communications Commission", :org_type => "governmental")
+      org.should be_an_instance_of(Organization)
+      org.name.should == "Federal Communications Commission"
     end
 
-    it "should return correct search result" do
-      @organizations = Organization.search("Federal")
-      @organizations.size.should == 2
-      @organizations.first.name.should == "Federal Communications Commission"
+    it "should raise BadRequest when a bad URL is passed in" do
+      executing do 
+        Organization.create(:name => "Bad Org", :url => "htt:p//jlkj!3", :org_type => "governmental")
+      end.should raise_error(BadRequest)
+    end
+  end
+
+  describe ".destroy" do
+    before do
+      @organization = Organization.create(:name => "Federal Election Commission", :org_type => "governmental")
+    end
+
+    it "should destroy an existing organization" do
+      Organization.destroy(@organization.id).should be_true
+    end
+
+    it "should raise NotFound when attempting to destroy non-existing organization" do
+      executing do
+        Organization.destroy(mangle(@organization.id))
+      end.should raise_error(NotFound)
     end
   end
 
@@ -81,17 +99,15 @@ describe Organization do
     end
   end
 
-  describe ".create" do
-    it "should create a new organization when valid params are passed in" do
-      org = Organization.create(:name => "Federal Communications Commission", :org_type => "governmental")
-      org.should be_an_instance_of(Organization)
-      org.name.should == "Federal Communications Commission"
+  describe ".search" do
+    before do
+      create_3_organizations
     end
 
-    it "should raise BadRequest when a bad URL is passed in" do
-      executing do 
-        Organization.create(:name => "Bad Org", :url => "htt:p//jlkj!3", :org_type => "governmental")
-      end.should raise_error(BadRequest)
+    it "should return correct search result" do
+      @organizations = Organization.search("Federal")
+      @organizations.size.should == 2
+      @organizations.first.name.should == "Federal Communications Commission"
     end
   end
 
@@ -104,22 +120,6 @@ describe Organization do
       organization = Organization.update(@organization.id, { :url => "http://fec.gov/" })
       organization.should be_an_instance_of(Organization)
       organization.url.should == "http://fec.gov/"
-    end
-  end
-
-  describe ".destroy" do
-    before do
-      @organization = Organization.create(:name => "Federal Election Commission", :org_type => "governmental")
-    end
-
-    it "should destroy an existing organization" do
-      Organization.destroy(@organization.id).should be_true
-    end
-
-    it "should raise NotFound when attempting to destroy non-existing organization" do
-      executing do
-        Organization.destroy(mangle(@organization.id))
-      end.should raise_error(NotFound)
     end
   end
 
