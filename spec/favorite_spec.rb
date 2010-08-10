319e933ddd39caeb8736a865449eb68833aa3506
David James <djames@sunlightfoundation.com>
2010-08-10 16:07:13 -0400

    minor: whitespace

--- spec/favorite_spec.rb
+++ spec/favorite_spec.rb
@@ -21,16 +21,13 @@ describe Favorite do
   end
 
   describe ".create" do
-
     it "should create a favorite when valid params are passed in" do
       refreshed_user = User.get(@user.id)
       refreshed_user.favorites.first.title.should == "Some FCC Data"
     end
-
-  end # describe ".create"
+  end
 
   describe ".get" do
-
     it "should return a favorite" do
       favorite = Favorite.get(@favorite.id)
       favorite.should be_an_instance_of(Favorite)
@@ -42,23 +39,18 @@ describe Favorite do
         Favorite.get(mangle(@favorite.id))
       end.should raise_error(NotFound)
     end
-
-  end # describe ".get"
+  end
 
   describe ".all" do
-
     it "should return an enumeration of favorites" do
       favorites = Favorite.all(:user_id => @user.id)
       favorites.each do |o|
         o.should be_an_instance_of(Favorite)
       end
     end
-
-  end # describe ".all"
-
+  end
 
   describe ".destroy" do
-
     it "should destroy an existing favorite as an admin" do
       Favorite.destroy(@favorite.id).should be_true
     end
@@ -74,7 +66,6 @@ describe Favorite do
         Favorite.destroy(mangle(@favorite.id))
       end.should raise_error(NotFound)
     end
-
-  end # describe ".destroy"
+  end
 
 end
