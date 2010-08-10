3f0a3e895da1aa6eb8a077a55fdaccd677f785bf
David James <djames@sunlightfoundation.com>
2010-08-10 16:51:24 -0400

    minor: alphabetize

--- spec/download_spec.rb
+++ spec/download_spec.rb
@@ -23,10 +23,31 @@ describe Download do
     )
   end
 
+  describe ".all" do
+    it "should return an enumeration of downloads" do
+      downloads = Download.all(:source_id => @source.id)
+      downloads.each do |o|
+        o.should be_an_instance_of(Download)
+      end
+    end
+  end
+
   describe ".create" do
     it "should create a download when valid params are passed in" do
       refreshed_source = Source.get(@source.id)
       refreshed_source.downloads.first.url.should == "http://somedata.gov/test.csv"
+    end
+  end
+
+  describe ".destroy" do
+    it "should destroy an existing download as an admin" do
+      Download.destroy(@download.id).should be_true
+    end
+
+    it "should raise NotFound when attempting to destroy non-existing download" do
+      executing do
+        Download.destroy(mangle(@download.id))
+      end.should raise_error(NotFound)
     end
   end
 
@@ -44,32 +65,11 @@ describe Download do
     end
   end
 
-  describe ".all" do
-    it "should return an enumeration of downloads" do
-      downloads = Download.all(:source_id => @source.id)
-      downloads.each do |o|
-        o.should be_an_instance_of(Download)
-      end
-    end
-  end
-
   describe ".update" do
     it "should update an existing download with valid params" do
       Download.update(@download.id, :preview => "10,11,12,13")
       refreshed_download = Download.get(@download.id)
       refreshed_download.preview.should == "10,11,12,13"
-    end
-  end
-
-  describe ".destroy" do
-    it "should destroy an existing download as an admin" do
-      Download.destroy(@download.id).should be_true
-    end
-
-    it "should raise NotFound when attempting to destroy non-existing download" do
-      executing do
-        Download.destroy(mangle(@download.id))
-      end.should raise_error(NotFound)
     end
   end
 
