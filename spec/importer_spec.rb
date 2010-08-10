b1a5713d7cebad7bc1255f8af4a7b5965cc64664
David James <djames@sunlightfoundation.com>
2010-08-10 16:51:21 -0400

    minor: alphabetize

--- spec/importer_spec.rb
+++ spec/importer_spec.rb
@@ -16,19 +16,13 @@ describe Importer do
     ]
   end
 
-  describe ".get" do
-    it "should return an importer as a basic user" do
-      importer = DataCatalog.with_key(@user.primary_api_key) do
-        Importer.get(@importers[0].id)
+  describe ".all" do
+    it "should return an enumeration of 2 importers" do
+      importers = Importer.all
+      importers.length.should == 2
+      importers.each do |o|
+        o.should be_an_instance_of(Importer)
       end
-      importer.should be_an_instance_of(Importer)
-      importer.id.should == @importers[0].id
-    end
-
-    it "should raise NotFound if no importer exists" do
-      executing do
-        Importer.get(mangle(@importers[0].id))
-      end.should raise_error(NotFound)
     end
   end
 
@@ -56,33 +50,6 @@ describe Importer do
     end
   end
 
-  describe ".all" do
-    it "should return an enumeration of 2 importers" do
-      importers = Importer.all
-      importers.length.should == 2
-      importers.each do |o|
-        o.should be_an_instance_of(Importer)
-      end
-    end
-  end
-
-  describe ".update" do
-    it "a basic user should be unauthorized" do
-      executing do
-        DataCatalog.with_key(@user.primary_api_key) do
-          Importer.update(@importers[0].id, :name => "International Bat Registry")
-        end
-      end.should raise_error(Unauthorized)
-    end
-
-    it "should update an existing importer with valid params" do
-      @importers[0].name.should == "National Data Registry of Bats"
-      Importer.update(@importers[0].id, :name => "International Bat Registry")
-      importer = Importer.get(@importers[0].id)
-      importer.name.should == "International Bat Registry"
-    end
-  end
-
   describe ".destroy" do
     it "should destroy an existing importer as an admin" do
       Importer.destroy(@importers[0].id).should be_true
@@ -100,6 +67,39 @@ describe Importer do
       executing do
         Importer.destroy(mangle(@importers[0].id))
       end.should raise_error(NotFound)
+    end
+  end
+
+  describe ".get" do
+    it "should return an importer as a basic user" do
+      importer = DataCatalog.with_key(@user.primary_api_key) do
+        Importer.get(@importers[0].id)
+      end
+      importer.should be_an_instance_of(Importer)
+      importer.id.should == @importers[0].id
+    end
+
+    it "should raise NotFound if no importer exists" do
+      executing do
+        Importer.get(mangle(@importers[0].id))
+      end.should raise_error(NotFound)
+    end
+  end
+
+  describe ".update" do
+    it "a basic user should be unauthorized" do
+      executing do
+        DataCatalog.with_key(@user.primary_api_key) do
+          Importer.update(@importers[0].id, :name => "International Bat Registry")
+        end
+      end.should raise_error(Unauthorized)
+    end
+
+    it "should update an existing importer with valid params" do
+      @importers[0].name.should == "National Data Registry of Bats"
+      Importer.update(@importers[0].id, :name => "International Bat Registry")
+      importer = Importer.get(@importers[0].id)
+      importer.name.should == "International Bat Registry"
     end
   end
 
