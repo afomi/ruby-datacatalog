dcfdb17be9062e2e07adde5f934a6bfa0e7d886d
Ryan Wold <wold@afomi.com>
2010-08-13 09:59:55 -0700

    minor refactor (tests incomplete)
    test for @user permissions

--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -2,38 +2,37 @@ require File.dirname(__FILE__) + '/spec_helper'
 include DataCatalog
 
 describe Category do
-  
+
   before do
     setup_api
     clean_slate
     @user = User.create(
       :name    => "Ted Smith",
-      :email   => "ted@email.com",
-      :curator => true)
+      :email   => "ted@email.com")
 
-    DataCatalog.with_key(@user.primary_api_key) do
-      @category  = Category.create(:name => "TestCategory1")
-      @category2 = Category.create(:name => "TestCategory2")
-    end
+    @categories = [
+      Category.create(:name => "Category-1"),
+      Category.create(:name => "Category-2")
+    ]
   end
-  
+
   describe ".create" do
     it "should create a new category when valid params are passed in" do
-      @category.should be_an_instance_of(Category)
-      @category.name.should == "TestCategory1"
+      @categories[0].should be_an_instance_of(Category)
+      @categories[0].name.should == "Category-1"
     end
   end
 
   describe ".get" do
     it "should return a category" do
-      category = Category.get(@category.id)
+      category = Category.get(@categories[0].id)
       category.should be_an_instance_of(Category)
-      category.name.should == "TestCategory1"
+      category.name.should == "Category-1"
     end
-    
+
     it "should raise NotFound if no category exists" do
       executing do
-        Category.get(mangle(@category.id))
+        Category.get(mangle(@categories[0].id))
       end.should raise_error(NotFound)
     end
   end
@@ -43,36 +42,34 @@ describe Category do
       categories = Category.all
       categories.first.should be_an_instance_of(Category)
       [1, 2].each_with_index do |nth, i|
-        categories[i].name.should == "TestCategory#{nth}"
+        categories[i].name.should == "Category-#{nth}"
       end
     end
   end
 
   describe ".update" do
     it "should update an existing category with valid params" do
-      DataCatalog.with_key(@user.primary_api_key) do
-        Category.update(@category.id, :name => "TestCategoryUpdate")
-      end
-      
-      refreshed_category = Category.get(@category.id)
+      Category.update(@categories[0].id, :name => "TestCategoryUpdate")
+
+      refreshed_category = Category.get(@categories[0].id)
       refreshed_category.name.should == "TestCategoryUpdate"
     end
   end
 
   describe ".destroy" do 
     it "should destroy an existing category as an admin" do
-      Category.destroy(@category.id).should be_true
+      Category.destroy(@categories[0].id).should be_true
     end
     
 #    it "should not destroy an existing category as the user" do
 #      DataCatalog.with_key(@user.primary_api_key) do
-#        Category.destroy(@category.id).should be_false
+#        Category.destroy(@categories[0].id).should be_false
 #      end
 #    end
     
     it "should raise NotFound when attempting to destroy non-existing category" do
       executing do
-        Category.destroy(mangle(@category.id))
+        Category.destroy(mangle(@categories[0].id))
       end.should raise_error(NotFound)
     end
   end
