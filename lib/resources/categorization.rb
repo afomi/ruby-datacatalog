e33f97f46e8d81d82e3a093ad325d6489e1b1115
Ryan Wold <wold@afomi.com>
2010-08-13 09:59:18 -0700

    add Categorizations  (incomplete Test)
    test to check for @user permissions

---
+++ lib/resources/categorization.rb
@@ -0,0 +1,33 @@
+module DataCatalog
+
+  class Categorization < Base
+    
+    def self.all(conditions={})
+      cursor(uri, query_hash(conditions))
+    end
+
+    def self.create(params={})
+      one(http_post(uri, :body => params))
+    end
+
+    def self.destroy(id)
+      one(http_delete(uri(id)))
+    end
+    
+    def self.get(id)
+      one(http_get(uri(id)))
+    end
+    
+    def self.update(id, params={})
+      one(http_put(uri(id), :body => params))
+    end
+
+    # == Helpers
+    
+    def self.uri(id=nil)
+      "/categorizations/#{id}"
+    end
+
+  end
+
+end
---
+++ spec/categorization_spec.rb
@@ -0,0 +1,103 @@
+require File.dirname(__FILE__) + '/spec_helper'
+include DataCatalog
+
+describe Categorization do
+
+  before do
+    setup_api
+    clean_slate
+    @user = User.create(
+      :name    => "Ted Smith",
+      :email   => "ted@email.com")
+      
+    @sources = [
+      Source.create(
+        :title       => "Some FCC Data",
+        :url         => "http://fcc.gov/somedata.csv",
+        :source_type => "dataset"
+      ),
+      Source.create(
+        :title       => "Some Other Local Data",
+        :url         => "http://local.gov/currentdata.xml",
+        :source_type => "dataset"
+      )
+    ]
+    
+    @categories = [
+      Category.create(:name => "Category-0"),
+      Category.create(:name => "Category-1")
+    ]
+
+    DataCatalog.with_key(@user.primary_api_key) do
+      @categorizations = [
+        Categorization.create(:source_id => @sources[0].id, :category_id => @categories[0].id),
+        Categorization.create(:source_id => @sources[0].id, :category_id => @categories[1].id),
+        Categorization.create(:source_id => @sources[1].id, :category_id => @categories[0].id),
+      ]
+    end
+  end
+
+  describe ".create" do
+    it "should create a new categorization when valid params are passed in" do
+      @categorizations[0].should be_an_instance_of(Categorization)
+    end
+  end
+
+  describe ".get" do
+    it "should return a categorization" do
+      categorization = Categorization.get(@categorizations[0].id)
+      categorization.should be_an_instance_of(Categorization)
+    end
+
+    it "should raise NotFound if no categorization exists" do
+      executing do
+        Categorization.get(mangle(@categorizations[0].id))
+      end.should raise_error(NotFound)
+    end
+  end
+
+  describe ".all" do
+    it "should return a set of all categorizations" do
+      categorizations = Categorization.all
+      categorizations.each do |categorization|
+        categorization.should be_an_instance_of(Categorization)
+      end
+      ids = categorizations.map do |categorization|
+        categorization.id.to_s
+      end
+      ids.uniq.length.should == 3
+    end
+  end
+
+  describe ".update" do
+    it "should update an existing categorization as admin" do
+      Categorization.update(@categorizations[0].id, :category_id => @categories[1].id)
+
+      refreshed_Categorization = Categorization.get(@categorizations[0].id)
+      refreshed_Categorization.category_id.should == @categories[1].id
+    end
+    
+#    it "should update an exisiting categorization as user" do
+#      DataCatalog.with_key(@user.primary_api_key) do
+#        Categorization.update(@categorizations[0].id, :category_id => @categories[1].id)
+#      end
+
+#      refreshed_Categorization = Categorization.get(@categorizations[0].id)
+#      refreshed_Categorization.category_id.should == @categories[1].id
+#    end
+
+  end
+
+  describe ".destroy" do 
+    it "should destroy an existing categorization" do
+      Categorization.destroy(@categorizations[0].id).should be_true
+    end
+
+    it "should raise NotFound when attempting to destroy non-existing categorization" do
+      executing do
+        Categorization.destroy(mangle(@categorizations[0].id))
+      end.should raise_error(NotFound)
+    end
+  end
+end
+
